33fb4d6468ab4293e09ca03645ca62a41f6ed520
softdeveloper23 <brannongarrett77@gmail.com>
2025-05-08 18:26:41 +0200

    Updated code

--- student_database.py
+++ student_database.py
@@ -1,7 +1,12 @@
-def add_student():
-    ...
-def print_student():
-    ...
+def add_student(students : dict, name : str) -> None:
+    students[name] = "no completed courses"
+
+def print_student(students : dict, name : str) -> None:
+    if name not in students:
+        print(f"{name}: no such person in the database")
+    else:
+        print(f"{name}:")
+        print(f"\t{students[name]}")
 if __name__ == "__main__":
     students = {}
     add_student(students, "Peter")
