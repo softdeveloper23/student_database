7143c8202183ec13dcf709cadb2e4206d42fa2f7
softdeveloper23 <brannongarrett77@gmail.com>
2025-05-09 11:50:35 +0200

    Updated add_course function and changed average to .1f

--- student_database.py
+++ student_database.py
@@ -19,17 +19,20 @@ def print_student(students : dict, name : str) -> None:
                 total_grade += grade
 
             average = total_grade / len(courses) if courses else 0
-            print(f" average grade {average}")
+            print(f" average grade {average:.1f}")
 
 def add_course(students: dict, name : str, course: tuple) -> None:
     if name in students:
         if students[name] == "no completed courses":
             students[name] = []
-        students[name].append(course)
+        if course[1] != 0:
+            students[name].append(course)
 
 if __name__ == "__main__":
     students = {}
     add_student(students, "Peter")
     add_course(students, "Peter", ("Introduction to Programming", 3))
     add_course(students, "Peter", ("Advanced Course in Programming", 2))
+    add_course(students, "Peter", ("Data Structures and Algorithms", 0))
+    add_course(students, "Peter", ("Introduction to Programming", 2))
     print_student(students, "Peter")
